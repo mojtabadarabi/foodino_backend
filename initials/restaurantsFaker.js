f5ca3519166e246970c950d0c75bb68fe65d0f68
Mojtaba darabi <darabi.dev@gmail.com>
2024-01-14 10:10:42 -0800

    add restaurant approval

---
+++ initials/restaurantsFaker.js
@@ -0,0 +1,14 @@
+const roles = require('./restaurants.js');
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db('foodino');
+  dbo.collection("restaurants").insertMany(roles, { ordered: true }, function(err, result) {
+    if (err) throw err;
+    db.close();
+  });
+});
+
